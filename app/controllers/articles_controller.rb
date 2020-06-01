55fac3c1310a7f3a00de0acc9f0e893d85e887b2
Taha Abbasi <taha.abbasi88@gmail.com>
2020-06-01 00:51:09 -0400

    DRY the aritcle controller, added partials _form, _messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
   def show
-    @article = Article.find(params[:id])
   end
   
   def index
@@ -12,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article as created successfully"
       redirect_to article_path(@article)
@@ -25,19 +25,27 @@ class ArticlesController < ApplicationController
     end
   end
 
-    def update
-      @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
-        flash[:notice] = "Article was updated successfully."
-        redirect_to article_path(@article)
-      else
-        render 'edit'
-      end 
-    end
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end 
+  end
 
-    def destroy
-      @article = Article.find(params[:id])
-      @article.destroy
-      redirect_to articles_path
-    end
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
