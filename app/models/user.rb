0162f9dd7b35deeab16480e53696736f69304e7a
Taha Abbasi <taha.abbasi88@gmail.com>
2020-06-10 08:53:02 -0400

    changing the state of an object, before saving it to the DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase}
+  # change the state of an object before saving to DB 
+  # in the above line self is referencing any object for the user class, therefore when it comes to emails - before saving it DB apply downcase and then save
   has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
