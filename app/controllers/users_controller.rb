e94ff9460d2e7eb17586b0b9ef46bfb029c1a9bb
Taha Abbasi <taha.abbasi88@gmail.com>
2020-06-20 12:33:18 -0400

    added before_action for user controller, and current user and logged in predicate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     # adding an instance variable below, as articles are not going to show on user page without it 
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have now signed up"
       redirect_to articles_path
     else
@@ -26,11 +27,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "User was updated successfully."
       redirect_to @user
@@ -42,4 +41,7 @@ class UsersController < ApplicationController
   private def user_params
     params.require(:user).permit(:username, :email, :password)
   end 
+  private def set_user
+    user = User.find(params[:id])
+  end
 end 
